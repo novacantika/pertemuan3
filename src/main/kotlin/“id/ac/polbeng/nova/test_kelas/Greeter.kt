7e0be20a966ea942d006deffdc6bfb8ff380fc5d
Asus VivoBook <novacntikafitri@gmail.com>
2022-03-25 10:24:57 +0700

    Kelas dapat diumpamakan seperti spesifikasi atau blueprint (cetakan) untuk membuat sebuah objek

---
+++ src/main/kotlin/“id/ac/polbeng/nova/test_kelas/Greeter.kt
@@ -0,0 +1,14 @@
+package `“id`.ac.polbeng.nova.test_kelas
+
+class Greeter {
+    var text: String = ""
+    fun greet() {
+        println("Hello object world!")
+    }
+    fun greet(name: String){
+        println("$text $name")
+    }
+    fun with_ret_val(name: String): String {
+        return "$text $name"
+    }
+}
---
+++ src/main/kotlin/“id/ac/polbeng/nova/test_kelas/GreeterTest.kt
@@ -0,0 +1,11 @@
+package `“id`.ac.polbeng.nova.test_kelas
+
+fun main() {
+    val greeter = Greeter()
+    greeter.greet()
+    greeter.text = "HI"
+    greeter.greet("Nova")
+    greeter.greet("Eka")
+    greeter.text =("Hello Programer")
+    println(greeter.with_ret_val("Gina"))
+}
